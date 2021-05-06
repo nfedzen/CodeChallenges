a43b7c6125a19b3a826bdcf853834b591d746d9a
Nathan Fedzen <nathanfedzen@gmail.com>
2021-05-05 21:13:46 -0600

    finished smallest distance challenge

--- Coding-Challenges/smallestDistance/smallestDistance.js
+++ Coding-Challenges/smallestDistance/smallestDistance.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
 const smallestDistance = (words, string) => {
+  let wordsSplit = words.split(' ')
+  let stringSplit = string.split(' ')
+  let firstWordIndex = 0
+  let secondWordIndex = 0
 
+  for(let i = 0; i < stringSplit.length; i++){
+    if(wordsSplit[0] === stringSplit[i]){
+      firstWordIndex = i
+    }
+    if(wordsSplit[1] === stringSplit[i]){
+      secondWordIndex = i
+    }
+  }
+  let distance = (secondWordIndex - firstWordIndex) - 1
+  return distance
 };
 
-console.log(smallestDistance("hello world", "dog cat hello cat dog dog hello cat world")); // 1
+console.log(smallestDistance("hello world", "dog cat hello cat dog dog cat world")); // 1
