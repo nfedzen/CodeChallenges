522b616ed62d065230519fd07f1a9d0d47493a90
Nathan Fedzen <nathanfedzen@gmail.com>
2021-05-08 09:25:58 -0600

    completed isPrime challenge

---
+++ Coding-Challenges/isPrime/isPrime.js
@@ -0,0 +1,14 @@
+
+
+const isPrime = (number) => {
+  if(number === 2) return true
+  for(let i = 2; i < number; i++){
+    if(number % i === 0){
+      return false
+    }
+  }
+  return true
+}
+
+
+console.log(isPrime(1709))
